1328c218d8cdcf950d504f459024f77c646dd6f2
Julien <julien.erbland@epfl.ch>
2023-03-06 20:49:42 +0100

    Etape 3: sampleDecoder v1 et creation de powerComputer

---
+++ src/ch/epfl/javions/demodulation/PowerComputer.java
@@ -0,0 +1,38 @@
+package ch.epfl.javions.demodulation;
+
+import ch.epfl.javions.Preconditions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+public class PowerComputer {
+    private InputStream stream;
+
+    private final int batchSize;
+
+    private int indexAncienEchatillon;
+
+    private int[] tabCirculaire = new int[8];
+
+    public PowerComputer(InputStream stream, int batchSize){
+        Preconditions.checkArgument(batchSize>0 && ((batchSize%8)==0));
+        Objects.requireNonNull(stream);
+
+        this.stream=stream;
+        this.batchSize=batchSize;
+    }
+
+    public int readBatch(int[] batch) throws IOException {
+
+        return 0;
+    }
+
+    private int calculPuissanceEchantillon(int x7, int x6, int x5, int x4, int x3, int x2, int x1, int x0){
+        int p1 = x6 - x4 + x2 - x0;
+        int p2 = x7 - x5 + x3 - x1;
+        return p1*p1 - p2*p2;
+    }
+
+
+}
