512c2f66c0ce6a4328edf380b00d12a7a9844de4
Julien <julien.erbland@epfl.ch>
2023-03-15 14:14:37 +0100

    Etape 4 marche mais pas opti. 5.3 chez Julien

--- test/ch/epfl/javions/demodulation/AdsbDemodulatorTest.java
+++ test/ch/epfl/javions/demodulation/AdsbDemodulatorTest.java
@@ -13,13 +13,16 @@ class AdsbDemodulatorTest {
 
     @Test
     void testVitesse() {
+        //int counter=0;
         String f = "resources\\samples_20230304_1442.bin";
         try (InputStream s = new FileInputStream(f)) {
             AdsbDemodulator d = new AdsbDemodulator(s);
             RawMessage m;
             while ((m = d.nextMessage()) != null){
                 System.out.println(m);
+                //++counter;
             }
+            //System.out.println(counter);
         } catch (IOException e) {
             e.printStackTrace();
         }
