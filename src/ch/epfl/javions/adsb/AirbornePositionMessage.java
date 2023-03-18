7b8c34c99e0da35c292bcdac29cf3f6c26c5dd07
Julien <julien.erbland@epfl.ch>
2023-03-18 19:35:06 +0100

    Etape 5 : squelette des classes et fonctions

--- src/ch/epfl/javions/adsb/AirbornePositionMessage.java
+++ src/ch/epfl/javions/adsb/AirbornePositionMessage.java
@@ -8,6 +8,7 @@ import java.util.Objects;
 public record AirbornePositionMessage(long timeStampNs, IcaoAddress icaoAddress,double altitude, int parity, double x, double y) implements Message{
 
     public AirbornePositionMessage{
+        //pour commit
         Objects.requireNonNull(icaoAddress);
         Preconditions.checkArgument(timeStampNs>=0 && (parity==0 || parity==0) && (x>=0 && x<1) && (y>=0 && y<1));
     }
