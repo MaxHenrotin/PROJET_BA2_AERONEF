5cc9ef54655ef7a487489b5dd85b81bb0fc9372d
MAX_H <max.henrotin@epfl.ch>
2023-03-21 16:50:48 +0100

    ajout tests Q0

--- test/ch/epfl/javions/demodulation/AdsbDemodulatorTest.java
+++ test/ch/epfl/javions/demodulation/AdsbDemodulatorTest.java
@@ -12,8 +12,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AdsbDemodulatorTest {
 
-    @Test
+    @Test   //test du prof
     void testVitesse() {
+        int counter = 0;
         String f = "resources\\samples_20230304_1442.bin";
         try (InputStream s = new FileInputStream(f)) {
             AdsbDemodulator d = new AdsbDemodulator(s);
@@ -21,7 +22,9 @@ class AdsbDemodulatorTest {
             while ((m = d.nextMessage()) != null){
                 System.out.println(m);
                 System.out.println(m.icaoAddress());
+                ++counter;
             }
+            assertEquals(384, counter);
         } catch (IOException e) {
             e.printStackTrace();
         }
