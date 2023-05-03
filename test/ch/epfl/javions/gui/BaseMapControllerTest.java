fc9c7d62e9d282a477cefdb21c1802346004682f
MAX_H <max.henrotin@epfl.ch>
2023-05-03 15:53:59 +0200

    ajout methode pretelechargement de tuiles (pas implementée)

--- test/ch/epfl/javions/gui/BaseMapControllerTest.java
+++ test/ch/epfl/javions/gui/BaseMapControllerTest.java
@@ -19,13 +19,12 @@ public final class BaseMapControllerTest extends Application {
 
     public void start(Stage primaryStage) throws IOException {
         Path tileCache = Path.of("tile-cache");
-        TileManager tm = new TileManager(tileCache, "tile.openstreetmap.org");
+        TileManager tm = new TileManager(tileCache, "a.tile.openstreetmap.fr/hot");  // "tile.openstreetmap.org"
         MapParameters mp = new MapParameters(17, 17_389_327, 11_867_430);
         BaseMapController bmc = new BaseMapController(tm, mp);
 
         BorderPane root = new BorderPane(bmc.pane());
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
-
     }
 }
