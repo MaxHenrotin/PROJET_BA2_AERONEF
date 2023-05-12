0f9ebc8edbf774c48500da5759f8dc35378d7f35
Julien <julien.erbland@epfl.ch>
2023-05-12 17:11:40 +0200

    Etape 11 : StatusLineController fait et début de main avec un fil d'execution mais marche pas

--- src/ch/epfl/javions/gui/Main.java
+++ src/ch/epfl/javions/gui/Main.java
@@ -11,8 +11,8 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.*;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TableView;
@@ -23,19 +23,17 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.net.URL;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Observable;
-import java.util.Spliterator;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 public final class Main extends Application {
-    private static ObservableList<RawMessage> allMessages = FXCollections.observableArrayList();
+
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    static List<RawMessage> readAllMessages(String fileName) throws IOException {
+    /*static void readAllMessages(String fileName,LongProperty messageCount) throws IOException {
         try (DataInputStream s = new DataInputStream(
                 new BufferedInputStream(
                         new FileInputStream(fileName)))){
@@ -48,16 +46,19 @@ public final class Main extends Application {
                 RawMessage rawMessage = RawMessage.of(timeStampNs,bytes);
                 if(rawMessage != null){
                     allMessages.add(rawMessage);
+                    messageCount.set(messageCount.longValue() + 1);
                 }
             }
         }
-        catch (EOFException e) { /* nothing to do */ }
-
-        return allMessages;
-    }
+        catch (EOFException e) { }
+    }*/
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        getParameters().getRaw();
+        ConcurrentLinkedDeque<RawMessage> allMessages = new ConcurrentLinkedDeque<>();
+        List<String> args = getParameters().getRaw();
+
         // … à compléter (voir TestBaseMapController)
         Path tileCache = Path.of("tile-cache");
         TileManager tileManager = new TileManager(tileCache, "tile.openstreetmap.org");  // "tile.openstreetmap.org"
@@ -80,6 +81,7 @@ public final class Main extends Application {
         //Crée la ligne affichant le nombre de messages et d'avions visibles
         StatusLineController statusLineController = new StatusLineController();
         IntegerProperty aircraftCount = statusLineController.aircraftCountPropertyProperty();
+        LongProperty messageCount = statusLineController.messageCountProperty();
         aircraftCount.bind(Bindings.size(states));
         Pane statusLine = statusLineController.pane();
 
@@ -92,29 +94,53 @@ public final class Main extends Application {
         statusAndTable.setCenter(tableView);
 
         SplitPane finalPane = new SplitPane(mapWithAircrafts,statusAndTable);
+        finalPane.setOrientation(Orientation.VERTICAL);
         primaryStage.setScene(new Scene(finalPane));
         primaryStage.setTitle("Javions");
         primaryStage.setMinWidth(800);
         primaryStage.setMinHeight(600);
         primaryStage.show();
 
-        var mi = readAllMessages("resources\\messages_20230318_0915.bin")
-                .iterator();
+        new Thread(() -> {
+            try (DataInputStream s = new DataInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream("resources\\messages_20230318_0915.bin")))){
 
-        // Animation des aéronefs
-        new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                try {
-                    for (int i = 0; i < 10; i += 1) {
-                        Message m = MessageParser.parse(mi.next());
-                        if (m != null) asm.updateWithMessage(m);
+                byte[] bytes = new byte[RawMessage.LENGTH];
+                while (true) {
+                    long timeStampNs = s.readLong();
+                    int bytesRead = s.readNBytes(bytes, 0, bytes.length);
+                    assert bytesRead == RawMessage.LENGTH;
+                    RawMessage rawMessage = RawMessage.of(timeStampNs,bytes);
+                    if(rawMessage != null){
+                        allMessages.add(rawMessage);
+                        messageCount.set(messageCount.longValue() + 1);
                     }
-                } catch (IOException e) {
-                    throw new UncheckedIOException(e);
                 }
-                asm.purge();
             }
-        }.start();
+            catch (EOFException e) { /* nothing to do */ } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            var mi = allMessages.iterator();
+
+            // Animation des aéronefs
+            new AnimationTimer() {
+                @Override
+                public void handle(long now) {
+                    try {
+                        for (int i = 0; i < 10; i += 1) {
+                            Message m = MessageParser.parse(mi.next());
+                            if (m != null) asm.updateWithMessage(m);
+                        }
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                    asm.purge();
+                }
+            }.start();
+            });
     }
 }
