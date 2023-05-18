6e8a611e0a8827c51227b361c85183e586441779
MAX_H <max.henrotin@epfl.ch>
2023-05-18 16:19:32 +0200

    mini modifs StatusLineController

--- src/ch/epfl/javions/gui/StatusLineController.java
+++ src/ch/epfl/javions/gui/StatusLineController.java
@@ -7,21 +7,37 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 
 public final class StatusLineController {
+
+    //===================================== Attributs privées statiques ================================================
+
+    private static final String PANE_STYLESHEETS = "status.css";
+    private static final String TEXT_FOR_AERONEF_COUNT = "Aéronefs visibles : ";
+    private static final String TEXT_FOR_MESSAGE_COUNT = "Messages reçus : ";
+
+
+    //===================================== Attributs privées ==========================================================
+
     private BorderPane pane;
     private IntegerProperty aircraftCountProperty;
     private LongProperty messageCountProperty;
 
+
+    //===================================== Méthodes publiques =========================================================
+
     public StatusLineController(){
         pane = new BorderPane();
+        pane.getStylesheets().add(PANE_STYLESHEETS);
 
         aircraftCountProperty = new SimpleIntegerProperty(0);
         Text aircraftCountText = new Text();
-        aircraftCountText.textProperty().bind(aircraftCountProperty.map(count -> "Aéronefs visibles : "+count.toString()));
+        aircraftCountText.textProperty().bind(
+                aircraftCountProperty.map(count -> TEXT_FOR_AERONEF_COUNT + count.toString()));
         pane.setLeft(aircraftCountText);
 
         messageCountProperty = new SimpleLongProperty(0L);
         Text messageCountText = new Text();
-        messageCountText.textProperty().bind(messageCountProperty.map(count -> "Messages reçus : "+count.toString()));
+        messageCountText.textProperty().bind(
+                messageCountProperty.map(count -> TEXT_FOR_MESSAGE_COUNT + count.toString()));
         pane.setRight(messageCountText);
     }
 
